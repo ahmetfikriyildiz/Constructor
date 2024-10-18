9efa7620154f4de3be7883df61191d3864de82f8
ahmet fikri yıldız <ahmet_6119@outlook.com.tr>
2024-10-18 11:50:13 +0300

    c

--- Constructor/Program.cs
+++ Constructor/Program.cs
@@ -1,15 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Constructor
 {
+//Bu örnekte sizlerden bir Bebek sınıfı tanımlamanız isteniyor.
+
+//Bebek sınıfının propertyleri -> Doğum Tarihi, Ad , Soyad
+
+//Bebek sınıfı için 2 adet constructor tanımlayınız.
+
+//1.si Default Constructor tarzında parametre almayan bir metot.
+
+//2.si Ad ve Soyad parametrelerini alarak newleme işlemi sırasında değerleri propertylere atayan alternatif constructor.
+
+//2 Constructor içinde ortak olarak bebek nesnesi oluşturulduğunda konsol ekranında bir " Ingaaaa " yazısı görülsün ve Doğum Tarihi o an olarak atansın.
+
+//Tanımlama sonrasında her iki constructor ile de birer bebek nesnesi tanımlayarak özelliklerini konsol ekranına yazdırınız.
+    public class Bebek
+    {
+        public DateTime DogumTarihi { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        public Bebek()
+        {
+            DogumTarihi = DateTime.Now;   
+            Console.WriteLine("Ingaaaa"); 
+        }
+
+        public Bebek(string ad, string soyad)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            DogumTarihi = DateTime.Now;   
+            Console.WriteLine("Ingaaaa");
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Ad: {Ad}, Soyad: {Soyad}, Doğum Tarihi: {DogumTarihi}");
+        }
+    }
     public class Program
     {
         static void Main(string[] args)
         {
+            Bebek bebek1 = new Bebek();
+            bebek1.Ad = "Ali";       
+            bebek1.Soyad = "Yılmaz"; 
+
+            
+            Bebek bebek2 = new Bebek("Ayşe", "Demir");
+
+            
+            Console.WriteLine("\nBebek 1 Bilgileri:");
+            bebek1.PrintInfo();
+
+            Console.WriteLine("\nBebek 2 Bilgileri:");
+            bebek2.PrintInfo();
+            Console.ReadKey();
         }
     }
 }
